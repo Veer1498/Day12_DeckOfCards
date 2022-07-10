76af7a63451281a57e57052ef4e8554658abd5bf
Veer1498 <singavarapuveerendra@gmail.com>
2022-07-10 19:47:13 +0530

    Added : Multiple Players Can Play Now

--- src/oopsproblems/Main.java
+++ src/oopsproblems/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Plase Enter How Many Players are There");
 		DeckOfCards dc = new DeckOfCards();
 		System.out.println("Welcome to Deck Of Cards Program");
 		System.out.println("********************************");
